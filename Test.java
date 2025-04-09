79469295aa7ddc4e96f86407bee40125d1d23303
big-basket <78880753+big-basket@users.noreply.github.com>
2025-04-09 18:31:22 +0100

    added more graph generation to double test them

--- Test.java
+++ Test.java
@@ -1,8 +1,17 @@
 public class Test {
     public static void main(String[] args) {
         // Test the RandomGraphGenerator
-        GraphDataStructure graph = RandomGraphGenerator.generateRandomGraph(10, 20);
-        System.out.println("Generated Graph: " + graph.displayGraph());
+        GraphDataStructure graph1 = RandomGraphGenerator.generateRandomGraph(10, 20);
+        System.out.println("Generated Graph: " + graph1.displayGraph());
+
+        GraphDataStructure graph2 = RandomGraphGenerator.generateRandomGraph(50, 80);
+        System.out.println("Generated Graph: " + graph2.displayGraph());
+
+        GraphDataStructure graph3 = RandomGraphGenerator.generateRandomGraph(100, 200);
+        System.out.println("Generated Graph: " + graph3.displayGraph());
+
+        GraphDataStructure graph4 = RandomGraphGenerator.generateRandomGraph(1000, 2000);
+        System.out.println("Generated Graph: " + graph4.displayGraph());
 
         // OptimisedSolution optimisedSolution = new OptimisedSolution();
         // optimisedSolution.solve(graph);
