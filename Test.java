0aba8e716fe816e31b47e9165ae5484899d6c5a6
big-basket <78880753+big-basket@users.noreply.github.com>
2025-05-09 10:54:51 +0100

    Refactor code structure for improved readability and maintainability

--- Test.java
+++ Test.java
@@ -1,7 +1,6 @@
 
 public class Test {
     public static void main(String[] args) {
-        GraphDataStructure graph2 = GraphDataStructure.readFromFile("random_graph.txt");
-        OptimisedSolutionMaxCut.solve(graph2);
+        
     }
 }
